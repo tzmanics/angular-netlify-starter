185eb4b675829ebf3cdb9c7b2c9778028725a17d
Tara Z. Manicsic <tzmanics@gmail.com>
2020-06-06 16:15:59 -0400

    removes starter content, adds scully and netlify setup

---
+++ scully.angular-netlify-starter.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "angular-netlify-starter",
+  outDir: './dist/static',
+  routes: {
+  }
+};
